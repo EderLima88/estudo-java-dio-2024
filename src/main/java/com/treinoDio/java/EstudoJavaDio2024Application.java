798d19934f190cd888bab7a2c9789f45c01b636c
EderLima88 <eder.ebl@gmail.com>
2024-08-05 09:59:22 -0300

    Inicio do desafio de projeto Spring DIO

---
+++ src/main/java/com/treinoDio/java/EstudoJavaDio2024Application.java
@@ -0,0 +1,13 @@
+package com.treinoDio.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJavaDio2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoJavaDio2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinoDio/java/EstudoJavaDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinoDio.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoJavaDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
